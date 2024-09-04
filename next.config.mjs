2febcc56e74ed1c888515c7927b2d22bfc03ec46
taeyuls <startez03@gmail.com>
2024-09-04 10:42:29 +0900

    speed-insights

--- next.config.mjs
+++ next.config.mjs
@@ -1,49 +1,27 @@
 import { withSentryConfig } from "@sentry/nextjs";
+import { withSpeedInsights } from "@vercel/speed-insights/next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 여기에 Next.js 관련 다른 설정들을 추가할 수 있습니다.
+  reactStrictMode: true,
+  // 기타 설정
 };
 
 const sentryOptions = {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
   org: "7ac79342789c",
   project: "javascript-nextjs",
-
-  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
   reactComponentAnnotation: {
     enabled: true,
   },
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware,
-  // otherwise reporting of client-side errors will fail.
   tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
   hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
   automaticVercelMonitors: true,
 };
 
-// Export the final configuration with Sentry integrated
-export default withSentryConfig(nextConfig, sentryOptions);
+// Sentry와 Speed Insights 통합 적용
+const configWithSentry = withSentryConfig(nextConfig, sentryOptions);
+
+export default withSpeedInsights(configWithSentry);
