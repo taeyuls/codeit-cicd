6337732e1d94d8e1e522dc4562ad20ebec01e061
taeyuls <startez03@gmail.com>
2024-09-04 11:32:07 +0900

    codecov

--- jest.config.ts
+++ jest.config.ts
@@ -10,6 +10,12 @@ const createJestConfig = nextJest({
 const config: Config = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+
+  // 커버리지 리포트 설정 추가
+  collectCoverage: true, // 커버리지 수집을 활성화
+  coverageDirectory: "coverage", // 커버리지 리포트를 저장할 디렉토리
+  coverageReporters: ["text", "lcov"], // 커버리지 리포트 포맷 설정
+  coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/jest.setup.ts"], // 커버리지 수집에서 제외할 경로 설정
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
